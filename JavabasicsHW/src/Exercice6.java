1e175d8c278d6bc9154aec989795de9c4e8c24e5
rusu_t <rusu.tatiana17@gmail.com>
2019-05-15 17:56:33 +0300

    commit from IJ

--- JavabasicsHW/src/Exercice6.java
+++ JavabasicsHW/src/Exercice6.java
@@ -1,7 +1,4 @@
 public class Exercice6 {
-    //Write a Java program which iterates the integers from 1 to 100.
-    // For multiples of three print "Fizz" instead of the number and print "Buzz"
-    // for the multiples of five. When number is divided by both three and five,
-    // print "fizz buzz"
+
 
 }
--- JavabasicsHW/src/Main.java
+++ JavabasicsHW/src/Main.java
@@ -52,6 +52,10 @@ public class Main {
 
 
         //Exercice 6
+        //Write a Java program which iterates the integers from 1 to 100.
+        // For multiples of three print "Fizz" instead of the number and print "Buzz"
+        // for the multiples of five. When number is divided by both three and five,
+        // print "fizz buzz"
         System.out.println("--Exercice 6 :");
         for(int i=0; i<=100; i++){
             if (i%3==0 && i%5==0)
